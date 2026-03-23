69c0236b9aa4a66640c3af0feb14dda998b723b7
vdoanhk7 <vdoanhk7@gmail.com>
2026-03-23 20:15:15 +0700

    initial Entity.java - Base Class

---
+++ project/src/main/java/com/nhom3/Entity.java
@@ -0,0 +1,17 @@
+package com.nhom3;
+
+public abstract class Entity {
+    protected int id;
+
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}

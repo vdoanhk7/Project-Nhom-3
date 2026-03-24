c00f5a6a6b050f5bcd70a24c7505116c1bd381bc
maiquanganh1606 <maiquanganh1606@gmail.com>
2026-03-24 10:44:31 +0700

    Create User.java

---
+++ project/src/main/java/com/nhom3/User.java
@@ -0,0 +1,25 @@
+package com;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}

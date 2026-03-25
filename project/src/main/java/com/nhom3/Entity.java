2e3e8f4576503eeea55816108ab477502621e427
vdoanhk7 <vdoanhk7@gmail.com>
2026-03-25 14:48:33 +0700

    entity delete type

--- project/src/main/java/com/nhom3/Entity.java
+++ project/src/main/java/com/nhom3/Entity.java
@@ -3,13 +3,11 @@ package com.nhom3;
 public abstract class Entity {
     // Moi thuc the deu phai co id xac dinh duy nhat
     protected String id;
-    protected String type;
 
     public Entity(String id) {
         this.id = id;
     }
 
     public abstract String getId();
-    public abstract String getType();
     public abstract void displayInfo();
 }

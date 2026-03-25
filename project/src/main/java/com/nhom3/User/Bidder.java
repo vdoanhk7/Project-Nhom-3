c64501d40abc823bd0f18f3bfb7bc3d2ed1ccad7
vdoanhk7 <vdoanhk7@gmail.com>
2026-03-25 16:39:39 +0700

    bidder error

--- project/src/main/java/com/nhom3/User/Bidder.java
+++ project/src/main/java/com/nhom3/User/Bidder.java
@@ -1,11 +1,11 @@
 package com.nhom3.User;
 
 public class Bidder extends User {
-    public Bidder(String id, String userName, String password, String name, String email, String phoneNumber) {
-        super("BD-" + id, userName, password, name, email, phoneNumber);
+    public Bidder(String userName, String password, String name, String email, String phoneNumber) {
+        super(userName, password, name, email, phoneNumber);
     }
     @Override
     public String getRoleName() {
-        return "BIDDER";    
+        return "Bidder";    
     }
 }

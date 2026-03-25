d5d6c54303fc8309b547faf9bc394795f5db66c8
p <phucv1901@gmail.com>
2026-03-25 15:39:10 +0700

    commit

---
+++ project/src/main/java/com/nhom3/Item/Item.java
@@ -0,0 +1,43 @@
+package com.nhom3;
+
+abstract class Item extends Entity{
+	private String iName;
+	private String info;
+	private double startPrice;
+	private double curHighest;
+	private LocalDate startAuction;
+	private LocalDate endAuction;
+	
+	protected Item(String id, String name, String info, double startPrice, double curHighest, LocalDate start, LocalDate end){
+		super(id);
+		this.iName = name;
+		this.info = info;
+		this.startPrice = startPrice;
+		this.curHighest = curHighest;
+		this.start = start;
+		this.end = end;
+	}
+	
+	protected void setName(){
+		
+	}
+	protected void setName(){
+
+	}
+	protected void setName(){
+
+	}
+	protected void setName(){
+
+	}
+	protected void setName(){
+
+	}
+	protected void setName(){
+
+	}
+	protected void setName(){
+
+	}
+
+}

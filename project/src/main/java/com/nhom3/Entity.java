02eb6569ffaa04fe2f1f2ca16f10aad2f892928f
vdoanhk7 <vdoanhk7@gmail.com>
2026-03-25 14:37:10 +0700

    entity - type

--- project/src/main/java/com/nhom3/Entity.java
+++ project/src/main/java/com/nhom3/Entity.java
@@ -3,6 +3,7 @@ package com.nhom3;
 public abstract class Entity {
     // Moi thuc the deu phai co id xac dinh duy nhat
     protected int id;
+    protected String type;
 
     public Entity(int id) {
         this.id = id;
@@ -14,5 +15,13 @@ public abstract class Entity {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }

347a78ec345550517ea4986dc23e3bb7a9efdf3f
vdoanhk7 <vdoanhk7@gmail.com>
2026-03-25 15:31:36 +0700

    update gitignore

--- project/src/main/java/com/nhom3/Auction.java
+++ project/src/main/java/com/nhom3/Auction.java
@@ -1,9 +1,13 @@
 package com.nhom3;
 
+import java.util.ArrayList;
+
+import com.nhom3.User.Seller;
+
 public class Auction{
+    private ArrayList<Seller> sellers;
 
-
+    public Auction() {
+        this.sellers = new ArrayList<>();
     }
-
-    
 }

eedc66b148eaaef20209eb74d14fbc86e2e229db
p <phucv1901@gmail.com>
2026-03-31 14:38:43 +0700

    push lai

--- project/src/main/java/com/nhom3/Item/Art.java
+++ project/src/main/java/com/nhom3/Item/Art.java
@@ -1,16 +1,9 @@
 package com.nhom3.Item;
-
 import java.time.LocalDate;
 
 public class Art extends Item{
 	// Constructor
 	public Art(String id, String iName, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
 		super(id, iName, info, startPrice, startAuction, endAuction);
-	}
-
-	@Override
-	public void displayInfo() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
 	}
 }
--- project/src/main/java/com/nhom3/Item/Item.java
+++ project/src/main/java/com/nhom3/Item/Item.java
@@ -1,40 +1,71 @@
 package com.nhom3.Item;
 import java.time.LocalDate;
-import com.nhom3.Entity;
 
-public abstract class Item extends Entity{
-	protected String name;
+abstract class Item extends Entity{
+	protected String iName;
 	protected String info;
 	protected double startPrice;
 	protected double curHighest;
 	protected LocalDate startAuction;
 	protected LocalDate endAuction;
 	
-	public Item(String id, String name, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
+	//Constructor Item 
+	public Item(String id, String iName, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
 		super(id);
-		this.name = name;
+		this.iName = iName;
 		this.info = info;
 		this.startPrice = startPrice;
+		// curHighest starting price is startPrice
 		this.curHighest = startPrice;
 		this.startAuction = startAuction;
 		this.endAuction = endAuction;
 	}
 
-	abstract public void setName(String newName);
-	abstract public String getName();
+	// Setter getter name 
+	public void setName(String iName){
+		this.iName = iName;
+	}
+	public String getName(){
+		return iName;
+	}
 	
-	abstract public void setInfo(String newInfo);
-	abstract public String getInfo();
+	// Setter getter info	
+	public void setInfo(String info){
+		this.info = info;
+	}
+	abstract public String getInfo(){
+		return info;
+	}
 		
-	abstract public void setStartPrice(double newStartPrice);
-	abstract public double getStartPrice();
+	// Setter getter startPrice
+	public void setStartPrice(double startPrice){
+		this.startPrice = startPrice;
+	}
+	public double getStartPrice(){
+		return startPrice;
+	}
 	
-	abstract public double getCurHighest();
-	abstract public void setCurHighest(double newCurHighest);
+	// Setter getter curHighest
+	public void setCurHighest(double curHighest){
+		this.curHighest = curHighest;
+	}
+	public double getCurHighest(){
+		return curHighest;
+	}
 
-	abstract public void setStartAuction(LocalDate newStartAuction);
-	abstract public LocalDate getStartAuction();
+	// Setter getter startAuction
+	public void setStartAuction(LocalDate startAuction){
+		this.startAuction = startAuction;
+	}
+	public LocalDate getStartAuction(){
+		return startAuction;
+	}
 
-	abstract public void setEndAuction(LocalDate newEndAuction);
-	abstract public LocalDate getEndAuction();
+	// Setter getter endAuction
+	public void setEndAuction(LocalDate endAuction){
+		this.endAuction = endAuction;
+	}
+	public LocalDate getEndAuction(){
+		return endAuction;
+	}
 }

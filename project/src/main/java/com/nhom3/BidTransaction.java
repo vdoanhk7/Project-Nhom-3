132631622673f4bbfd645dab889d1fa2e88ab893
maiquanganh1606 <maiquanganh1606@gmail.com>
2026-04-01 18:31:12 +0700

    Code Auction

--- project/src/main/java/com/nhom3/BidTransaction.java
+++ project/src/main/java/com/nhom3/BidTransaction.java
@@ -1,48 +1,22 @@
 package com.nhom3;
 
 import java.time.LocalDateTime;
+import com.nhom3.User.Bidder;
 
 class BidTransaction extends Entity{
     private String auctionId; // phien dau gia
-    private String bidderId; // ma nguoi dat
     private double bidAmount; // tien dat
     private LocalDateTime bidTime; // thoi gian dat
     private String note;
-    private String status;// trang thai dat
+    private Bidder bidder;
 
-    public BidTransaction(String id, String auctionId, String bidderId, double bidAmount,
-                          LocalDateTime bidTime, String note, String status) {
+    public BidTransaction(String id, String auctionId, Bidder bidder, double bidAmount,
+                          LocalDateTime bidTime, String note) {
         super(id);
         this.auctionId = auctionId;
-        this.bidderId = bidderId;
         this.bidAmount = bidAmount;
         this.bidTime = bidTime;
         this.note = note;
-        this.status = status;
-    }
-
-    public void setAuctionId(String auctionId) {
-        this.auctionId = auctionId;
-    }
-
-    public void setBidderId(String bidderId) {
-        this.bidderId = bidderId;
-    }
-
-    public void setBidAmount(double bidAmount) {
-        this.bidAmount = bidAmount;
-    }
-
-    public void setBidTime(LocalDateTime bidTime) {
-        this.bidTime = bidTime;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     @Override
@@ -62,27 +36,21 @@ class BidTransaction extends Entity{
         return bidTime;
     }
 
-    public String getBidderId() {
-        return bidderId;
+    public Bidder getBidder() {
+        return bidder;
     }
 
     public String getNote() {
         return note;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
     @Override
     public void displayInfo() {
         System.out.println("Mã giao dịch: " + id);
         System.out.println("Phiên đấu giá: " + auctionId);
-        System.out.println("Người đặt: " + bidderId);
         System.out.println("Giá đặt: " + bidAmount);
         System.out.println("Thời gian đặt: " + bidTime);
         System.out.println("Ghi chú: " + note);
-        System.out.println("Trạng thái: " + status);
     }
 
 }

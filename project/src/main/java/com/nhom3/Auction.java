14d43ce576e136396820af8111268115bcf36e2d
vdoanhk7 <vdoanhk7@gmail.com>
2026-04-01 17:05:09 +0700

    n

--- project/src/main/java/com/nhom3/Auction.java
+++ project/src/main/java/com/nhom3/Auction.java
@@ -2,19 +2,19 @@ package com.nhom3;
 import java.util.ArrayList;
 import com.nhom3.User.Seller;
 
-public class Auction{
+public class Auction extends Entity{
     private ArrayList<Seller> sellers;
 
-    public Auction() {
+    public Auction(String id) {
+        super(id);
         this.sellers = new ArrayList<>();
     }
 
-    public void display() {
-        for (Seller seller : sellers) {
-            for (Item item : seller.getItems()) {
-                item.displayInfo();
-                
-            }
-        }
+    @Override
+    public void displayInfo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
     }
+
+    
 }

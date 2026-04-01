681019110cb9377a8e83837fe9978c3c65e57941
vdoanhk7 <vdoanhk7@gmail.com>
2026-04-01 17:18:18 +0700

    toDo

--- project/src/main/java/com/nhom3/Item/Vehicle.java
+++ project/src/main/java/com/nhom3/Item/Vehicle.java
@@ -6,4 +6,11 @@ public class Vehicle extends Item{
 	public Vehicle(String id, String iName, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
 		super(id, iName, info, startPrice, startAuction, endAuction);
 	}
+
+	@Override
+	public void displayInfo() {
+		// TODO Auto-generated method stub
+		
+		throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
+	}
 }

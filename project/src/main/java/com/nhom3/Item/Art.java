678f98c1ef9580a335db6e795c57d34f0d4774c7
vdoanhk7 <vdoanhk7@gmail.com>
2026-04-01 17:20:14 +0700

    toDo

--- project/src/main/java/com/nhom3/Item/Art.java
+++ project/src/main/java/com/nhom3/Item/Art.java
@@ -6,4 +6,11 @@ public class Art extends Item{
 	public Art(String id, String iName, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
 		super(id, iName, info, startPrice, startAuction, endAuction);
 	}
+
+	@Override
+	public void displayInfo() {
+		// TODO Auto-generated method stub
+		//TODO
+		throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
+	}
 }
--- project/src/main/java/com/nhom3/Item/Electronics.java
+++ project/src/main/java/com/nhom3/Item/Electronics.java
@@ -6,4 +6,11 @@ public class Electronics extends Item{
 	public Electronics(String id, String iName, String info, double startPrice, LocalDate startAuction, LocalDate endAuction){
 		super(id, iName, info, startPrice, startAuction, endAuction);
 	}
+
+	@Override
+	public void displayInfo() {
+		// TODO Auto-generated method stub
+		
+		throw new UnsupportedOperationException("Unimplemented method 'displayInfo'");
+	}
 }

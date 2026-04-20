0998975ea904ff458dc1a9379dfc3ccde3bd115c
vdoanhk7 <vdoanhk7@gmail.com>
2026-04-20 14:32:54 +0700

    test: migrate AppTest from JUnit 4 to JUnit 5

--- project/src/test/java/com/nhom3/AppTest.java
+++ project/src/test/java/com/nhom3/AppTest.java
@@ -1,20 +1,18 @@
 package com.nhom3;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+// Xóa import org.junit.Test; cũ đi và thay bằng 2 dòng này:
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
